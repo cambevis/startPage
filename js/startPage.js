4c698af85e2f65720675cf0fda6cf3c47e7df587
Cam Bevis <cam@nobrainer2.com>
2020-06-23 18:39:14 -0500

    added feeds

--- js/startPage.js
+++ js/startPage.js
@@ -6,9 +6,32 @@ let feeds = [
         "url": "https://hackaday.com/blog/feed/"
     },
     {
+        "name": "Wired",
+        "url": "https://www.wired.com/feed/rss"
+    },
+    {
+        "name": "SciAm",
+        "url": "http://rss.sciam.com/ScientificAmerican-Global"
+    },
+    {
+        "name": "Engadget",
+        "url": "https://www.engadget.com/rss.xml"
+    },
+    {
         "name": "HackerNews",
         "url": "https://news.ycombinator.com/rss"
     }
+    ,
+    {
+        "name": "SlickDeals",
+        "url": "https://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&searchin=first&rss=1"
+    }
+    ,
+    {
+        "name": "Weather",
+        "url": "http://www.rssweather.com/zipcode/77410/rss.php"
+    }
+
 ];
 feeds.forEach(feedItem => {
     let feedURL = feedItem.url;
@@ -21,7 +44,14 @@ feeds.forEach(feedItem => {
         feed.items.slice(0, 5).forEach(function (entry) {
             mydiv = document.getElementById(feedItem.name);
             mydiv.insertAdjacentHTML('beforeend', '<a class="has-text-weight-bold"  href="' + entry.link + '" target="_blank">' + entry.title + '</a>');
-            mydiv.insertAdjacentHTML('beforeend', '<p>' + entry.content.slice(0, 125) + '...</p');
+            if (feedName != 'Weather') {
+                mydiv.insertAdjacentHTML('beforeend', '<p>' + entry.content.slice(0, 150) + '...</p');
+            }
+            else {
+                mydiv.insertAdjacentHTML('beforeend', '<p>' + entry.content + '</p');
+            }
+
+
         })
     })
 }
